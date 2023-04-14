abadd7501b8951950250b958e0f58b1db9b191f3
SakthiPoorani31 <130543014+SakthiPoorani31@users.noreply.github.com>
2023-04-14 11:02:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var sea,ship;
+var seaImg,shipImg;
+
+function preload(){
+  //uncomment the code to add animation to ship 
+
+  shipImg1 = loadAnimation("ship-1.png");
+  shipImg2 = loadAnimation("ship-1.png");
+  shipImg3 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  shipImg2 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  shipImg3 = loadAnimation("ship-2.png","ship-1.png","ship-2.png","ship-1.png");
+  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  
+  seaImg = loadImage("sea.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  background("blue");
+
+  // Moving background
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -7;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(100,250,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+
+  ship = createSprite(330,250,30,30);
+  ship.addAnimation("movingShip",shipImg2);
+  ship.scale =0.1;
+
+  ship = createSprite(220,220,10,10);
+  ship.addAnimation("movingShip",shipImg3);
+  ship.scale =0.10;
+  
+}
+
+function draw() {
+  background(0);
+  sea.velocityX = -3;
+
+  //uncomment code to reset the background
+  if(sea.x < 0){
+    //sea.x = 0;
+    //sea.x = sea.width;
+    sea.x = sea.width/8;
+    //sea.y = height;
+  }
+
+ 
+  drawSprites();
+}
